cb7887a896c0d1fb28e666122112b04d258a89b2
Deepankar <deepankar080892@gmail.com>
2020-08-18 03:15:26 +0530

    Create Dynamic Form Fields in React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import RemoveIcon from '@material-ui/icons/Remove';
+import AddIcon from '@material-ui/icons/Add';
+import Icon from '@material-ui/core/Icon';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((themes) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: themes.spacing(1),
+    },
+  },
+  button: {
+    margin: themes.spacing(1),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [inputFields, setInputFields] = useState([
+    { firstName: '', lastName: '' },
+    { firstName: '', lastName: '' },
+  ]);
+
+  const handleChangeInput = (index, event) => {
+    const values = [...inputFields];
+    values[index][event.target.name] = event.target.value;
+    setInputFields(values);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputFields);
+  };
+
+  const handleAddFields = () => {
+    setInputFields([...inputFields, { firstName: '', lastName: '' }]);
+  };
+  const handleRemoveFields = (index) => {
+    const values = [...inputFields];
+    values.splice(index, 1);
+    setInputFields(values);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <h1>Add New Member</h1>
+        <form className={classes.root} onSubmit={handleSubmit}>
+          {inputFields.map((inputField, index) => (
+            <div key={index}>
+              <TextField
+                name="firstName"
+                label="First Name"
+                value={inputField.firstName}
+                onChange={(event) => handleChangeInput(index, event)}
+                variant="filled"
+              />
+              <TextField
+                name="lastName"
+                label="Last Name"
+                value={inputField.lastName}
+                onChange={(event) => handleChangeInput(index, event)}
+                variant="filled"
+              />
+              <IconButton onClick={() => handleRemoveFields(index)}>
+                <RemoveIcon />
+              </IconButton>
+              <IconButton onClick={() => handleAddFields()}>
+                <AddIcon />
+              </IconButton>
+            </div>
+          ))}
+          <Button
+            className={classes.button}
+            variant="contained"
+            color="primary"
+            type="submit"
+            endIcon={<Icon>send</Icon>}
+            onClick={handleSubmit}
+          >
+            Send
+          </Button>
+        </form>
+      </Container>
     </div>
   );
 }
